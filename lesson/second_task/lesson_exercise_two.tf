a2743070663b1d67fe0d4a265d273fa4d889c031
[Piotr Pastuszek] <[piotr.pastuszek@bluesoft.com]>
2022-08-08 21:04:44 +0200

    Second task from lesson corrected

--- lesson/second_task/lesson_exercise_two.tf
+++ lesson/second_task/lesson_exercise_two.tf
@@ -1,12 +1,11 @@
-variable "id" {
-    type=string
+resource "random_pet" "animal" {
+    count = 3
 }
 
-resource "random_id" "default" {
-    byte_length = 8
-}
 resource "local_file" "backends" {
+    count = 3
+
     content  = templatefile("backends.tftpl", { ip_addrs = ["0.0.0.0", "8.8.8.8"],  port = 8080 })
-    filename = "${path.module}/backend-${random_id.default.hex}.conf"
+    filename = "${path.module}/backend-${random_pet.animal[count.index].id}.conf"
     file_permission = "0640"
 }
